6b93beaa5ee3de923698e828617a2f9d1dffd97a
Prathamesh84 <prathameshmore331@gmail.com>
2023-01-21 01:50:29 +0530

    counting sort added

--- main.cpp.cpp
+++ main.cpp.cpp
@@ -1,5 +1,22 @@
 #include <iostream>
+void bubble sort(){
+//
+}
+void quicksort(){
 
+//
+
+}
+
+void mergesort(){
+
+//
+}
+timepass
+void insertion sort(){
+
+    
+}
 int main(){
 
 std::cout<<"Hello world";
